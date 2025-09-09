3c2f62a2f05ac50071ebe97a6e0c9332f2a6c212
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-09-09 14:32:38 +0000

    mise à jour

--- middleware.ts
+++ middleware.ts
@@ -1,54 +1,46 @@
 import { NextRequest, NextResponse } from "next/server";
 import createIntlMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
-import { getToken } from "next-auth/jwt";
+import { auth } from "@/lib/auth";
 
 export const publicRoutes = ["/"];
 
+// Middleware d'internationalisation
 const intlMiddleware = createIntlMiddleware(routing);
 
 export default async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
+  const session = await auth();
 
-  const pathWithoutLocale =
-    pathname.replace(/^\/[a-z]{2}(?=\/|$)/, "") || "/";
-
-  const token = await getToken({
-    req,
-    secret: process.env.AUTH_SECRET,
-  });
+  const pathWithoutLocale = pathname.replace(/^\/[a-z]{2}(?=\/|$)/, '') || '/';
 
   if (process.env.NODE_ENV === "production") {
-    console.log(`Page dans le middleware : "${pathWithoutLocale}"`);
-    console.log(`Utilisateur connecté : ${token?.email ?? "non"}`);
-  }
+    console.log(`Page dans le middleware : "${pathWithoutLocale}"`)
+  };
 
   if (pathWithoutLocale.startsWith("/api/auth")) {
     return NextResponse.next();
   }
 
-  const isPublicPage = publicRoutes.some(
-    (route) =>
-      pathWithoutLocale === route || pathWithoutLocale.startsWith(route + "/")
-  );
+  const isPublicPage = publicRoutes.some(route => pathWithoutLocale === route || pathWithoutLocale.startsWith(route + '/'));
 
   if (isPublicPage) {
     return intlMiddleware(req);
-  }
+  } else {
+    if (!session) {
+      let callbackUrl = pathname;
+      if (req.nextUrl.search) {
+        callbackUrl += req.nextUrl.search;
+      }
 
-  if (!token) {
-    let callbackUrl = pathname;
-    if (req.nextUrl.search) {
-      callbackUrl += req.nextUrl.search;
+      const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+      const loginUrl = new URL(`/?callbackUrl=${encodedCallbackUrl}`, req.url);
+
+      return NextResponse.redirect(loginUrl);
     }
 
-    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
-    const loginUrl = new URL(`/?callbackUrl=${encodedCallbackUrl}`, req.url);
-
-    return NextResponse.redirect(loginUrl);
+    return intlMiddleware(req);
   }
-
-  return intlMiddleware(req);
 }
 
 export const config = {
@@ -56,5 +48,5 @@ export const config = {
     "/((?!.+\\.[\\w]+$|_next|_vercel).*)",
     "/",
     "/(api|trpc)(.*)",
-  ],
+  ]
 };
